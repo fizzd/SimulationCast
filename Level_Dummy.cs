3217b0ec813134bb331f0b36c80388f6fbd816bc
Hafiz <half.fizz@gmail.com>
2016-05-14 13:37:59 +0100

    sdfdf

---
+++ Level_Dummy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Level_Dummy : LevelBase
+{
+
+
+	/**
+ * INSERT NAME HERE
+ * bpm = 999;
+ */
+
+
+
+	public Level_Dummy () : base ()
+	{
+		//Properties to insert:
+
+		//songsUsed
+
+		//RankDescriptions
+
+		//WaitForExplode
+	}
+
+	override public void preactions ()
+	{
+		switch (conductor.barNumber)
+		{
+
+		}
+	}
+
+	override public void actions ()
+	{
+		switch (conductor.barNumber)
+		{
+
+		}
+			
+	}
+}
