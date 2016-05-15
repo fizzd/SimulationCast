f582b036a92c84a7b99600beea32c6207e6ef471
Hafiz <half.fizz@gmail.com>
2016-05-15 12:33:46 +0100

    asdfsdfdasdasd

---
+++ Simulation Cast Unity/Assets/updateTextToSlider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class updateTextToSlider : MonoBehaviour
+{
+
+	public Slider slider;
+	public Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		text.text = slider.value.ToString ();
+	}
+}
